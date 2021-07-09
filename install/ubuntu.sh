d22c2c2565bf8b13ade2c635302b4616ead612bb
chenghongyao <1253146300@qq.com>
2021-07-09 09:36:58 +0800

    remove npm install (install in nodejs)

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -33,7 +33,6 @@ sudo apt-get install -y python3-docutils
 if ! type "node" > /dev/null; then
     curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
     sudo apt-get install -y nodejs
-    sudo apt-get install -y npm
 else
     echo "[lin.vim] node already installed, skipping..."
 fi
